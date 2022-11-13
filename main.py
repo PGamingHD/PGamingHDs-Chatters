8ee8919c4b4d4ae5558fd0a650ac7e57561e39d5
PGamingHD <pontus.2003@hotmail.com>
2022-11-13 23:03:54 +0100

    Initiate TK and first method!
    Initiate TK and first method to handle all messages!

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 from network import send, connect, close;
 from datetime import datetime;
-import tkinter
+import tkinter;
+
+def message_handler(time, player, networkMsg):
+    currentDateAndTime = datetime.now().strftime("%H:%M:%S");
+
+top = tkinter.Tk();
+top.title("PGamingHDs Chatters");
