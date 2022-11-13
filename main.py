a35f4212a5575c5d2bb98ad9135d6855bdfb3fed
PGamingHD <pontus.2003@hotmail.com>
2022-11-13 23:07:12 +0100

    Initiate scrollbar, so that you can scroll through messages and also a message list inside of box for all messages to be stored!
    Initiate scrollbar, so that you can scroll through messages and also a message list inside of box for all messages to be stored!

--- main.py
+++ main.py
@@ -12,3 +12,9 @@ messages_frame = tkinter.Frame(top);
 my_msg = tkinter.StringVar();
 my_msg.set("Enter Message");
 scrollbar = tkinter.Scrollbar(messages_frame);
+
+msg_list = tkinter.Listbox(messages_frame, height=30, width=100, yscrollcommand=scrollbar.set);
+scrollbar.pack(side=tkinter.RIGHT, fill=tkinter.Y);
+msg_list.pack(side=tkinter.LEFT, fill=tkinter.BOTH);
+msg_list.pack();
+messages_frame.pack();
